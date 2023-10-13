494ceae06404d3e701cbce757b44ea4cc19deb47
lexandfr <alexfloresrayme@gmail.com>
2023-10-13 10:56:41 -0500

    first commit in course

---
+++ curso-manejo-estado-1/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { ClassState } from "./ClassState";
+import { UseState } from "./UseState";
+
+function App() {
+  return (
+    <>
+      <UseState />
+      <ClassState />
+    </>
+  );
+}
+
+export default App;
---
+++ curso-manejo-estado-1/src/UseState.jsx
@@ -0,0 +1,12 @@
+function UseState() {
+  return (
+    <div>
+      <h2>Eliminar UseState</h2>
+      <p>Por favor, escribe el código de seguridad.</p>
+      <input type="text" placeholder="escribe el godigo de seguridad" />
+      <button>Comprobar</button>
+    </div>
+  );
+}
+
+export { UseState };
